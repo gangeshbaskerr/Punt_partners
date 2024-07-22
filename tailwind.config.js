46e69b949971089154f83af275ed5a7159fceec2
Gangesh Basker <130077430+gangeshbaskerr@users.noreply.github.com>
2024-07-22 21:28:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0066b2',
+      },
+    },
+  },
+  plugins: [],
+};
+
